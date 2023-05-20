db9903f077f21abdd968ac048df36097af21a1ee
seya12 <lukas.seyr@gmx.at>
2023-05-20 10:20:37 +0200

    init

---
+++ src/test/java/com/auction/house/AuctionHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
